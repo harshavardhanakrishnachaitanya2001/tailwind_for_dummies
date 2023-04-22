468ca087209e2a843388849ed146080766b66fcb
G. H. Krishna Chaitanya <g.h.krishnachaitanya@Gs-Mac-mini.local>
2023-04-22 17:31:48 +0530

    Starting to add content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from './components/Header'
+import Content from './components/Content'
 
 const App = () => {
   return (
     <>
-       <Header /> 
+       <Header />
+       <div className='m-3'>
+        <Content />
+       </div>
     </>
   )
 }
---
+++ src/components/Content.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Content = () => {
+    return (
+        <>
+            <div className='space-y-3'>
+                {/* Intro */}
+                <div className='max-w-4xl text-lg p-2 '>
+                    <p>
+                    TailwindCSS is a CSS framework that makes styling webpages damn easy. Just add the 
+                    necessary classes and see the change.
+                    </p>
+                    <p>
+                        The best thing I've observed while working with CSS is, we get to see immediate changes. 
+                    </p>
+                </div>
+
+                <div>
+                    {/* Installation */}
+                    <h1 className='text-3xl font-bold tracking-wide bg-gradient-to-r from-blue-300 p-2 max-w-4xl rounded-lg to-white'>Setup</h1>
+                    <p>There won't be any changes in these steps to setup tailwindcss for the most part, but based on the framework you want to use tailwind for, there will be very slight changes. <a href='https://tailwindcss.com/docs/installation/framework-guides' target="_blank" className=' hover:underline text-blue-500 transition'>Click here to know the ways of adding tailwind to different frameworks</a></p>
+                    <p>I am telling the setup of tailwind, for websites made using create-react-app here </p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Content
