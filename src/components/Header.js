64e183db4c9a74cc9fbc31c73cc03fc0f74be343
G. H. Krishna Chaitanya <g.h.krishnachaitanya@Gs-Mac-mini.local>
2023-04-22 10:41:00 +0530

    Made the heading of the page

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {SiTailwindcss} from 'react-icons/si'
+
+const Header = () => {
+  return (
+    <div className='max-w-xl mx-auto'>
+        <div className=" flex justify-center space-x-2 shadow-lg shadow-gray-200 rounded-lg">
+            {/* Heading */}
+            <h1 className='py-3 font-semibold text-4xl'>TailwindCSS for Dummies</h1>
+
+            {/* Icon */}
+            <div className='mt-3'>
+                <SiTailwindcss className='text-blue-300 w-10 h-10 ' />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
+
